33a8a3f7940fd9a197c9131257998031ddef2ce7
Alessio Gravili <alessio@gravili.de>
2023-01-10 21:02:18 +0100

    Add serialize example

---
+++ demo/serialize-example/RichTextNodeFormat.ts
@@ -0,0 +1,64 @@
+//This copy-and-pasted from somewhere in lexical! I forgot where exactly, will check later
+
+export const DOM_ELEMENT_TYPE = 1;
+export const DOM_TEXT_TYPE = 3;
+
+// Reconciling
+export const NO_DIRTY_NODES = 0;
+export const HAS_DIRTY_NODES = 1;
+export const FULL_RECONCILE = 2;
+
+// Text node modes
+export const IS_NORMAL = 0;
+export const IS_TOKEN = 1;
+export const IS_SEGMENTED = 2;
+// IS_INERT = 3
+
+// Text node formatting
+export const IS_BOLD = 1;
+export const IS_ITALIC = 1 << 1;
+export const IS_STRIKETHROUGH = 1 << 2;
+export const IS_UNDERLINE = 1 << 3;
+export const IS_CODE = 1 << 4;
+export const IS_SUBSCRIPT = 1 << 5;
+export const IS_SUPERSCRIPT = 1 << 6;
+
+export const IS_ALL_FORMATTING =
+  IS_BOLD |
+  IS_ITALIC |
+  IS_STRIKETHROUGH |
+  IS_UNDERLINE |
+  IS_CODE |
+  IS_SUBSCRIPT |
+  IS_SUPERSCRIPT;
+
+// Text node details
+export const IS_DIRECTIONLESS = 1;
+export const IS_UNMERGEABLE = 1 << 1;
+
+// Element node formatting
+export const IS_ALIGN_LEFT = 1;
+export const IS_ALIGN_CENTER = 2;
+export const IS_ALIGN_RIGHT = 3;
+export const IS_ALIGN_JUSTIFY = 4;
+export const IS_ALIGN_START = 5;
+export const IS_ALIGN_END = 6;
+
+export const TEXT_TYPE_TO_FORMAT: Record<TextFormatType | string, number> = {
+    bold: IS_BOLD,
+    code: IS_CODE,
+    italic: IS_ITALIC,
+    strikethrough: IS_STRIKETHROUGH,
+    subscript: IS_SUBSCRIPT,
+    superscript: IS_SUPERSCRIPT,
+    underline: IS_UNDERLINE,
+  };
+
+  export type TextFormatType =
+  | 'bold'
+  | 'underline'
+  | 'strikethrough'
+  | 'italic'
+  | 'code'
+  | 'subscript'
+  | 'superscript';
---
+++ demo/serialize-example/types.ts
@@ -0,0 +1,21 @@
+export type SerializedLexicalEditorState = {
+    root: {
+        type: string,
+        format: string,
+        indent: number,
+        version: number,
+        children: SerializedLexicalNode[]
+    }
+}
+
+export type SerializedLexicalNode = {
+    children?: SerializedLexicalNode[],
+    direction: string,
+    format: number,
+    indent?: string|number,
+    type: string,
+    version: number,
+    style?: string,
+    mode?: string,
+    text?: string
+}
