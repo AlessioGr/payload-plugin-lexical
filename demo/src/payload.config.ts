763039f94c482a0297ba6da272b0895b7399903d
Alessio Gravili <alessio@gravili.de>
2023-04-07 20:48:04 +0200

    add react-i18next as alias to demo

--- demo/src/payload.config.ts
+++ demo/src/payload.config.ts
@@ -1,17 +1,17 @@
-import {buildConfig} from 'payload/config';
-import path from 'path';
+import { buildConfig } from "payload/config";
+import path from "path";
 //import LexicalPlugin from '../../src/index'
 //import LexicalPlugin from '../../dist/index'
-import Users from './collections/Users';
-import Media from './collections/Media';
-import RichText from './collections/RichText';
-import Lexical from './collections/Lexical';
-import LexicalCustomized from './collections/LexicalCustomized';
-import LexicalDebug from './collections/LexicalDebug';
+import Users from "./collections/Users";
+import Media from "./collections/Media";
+import RichText from "./collections/RichText";
+import Lexical from "./collections/Lexical";
+import LexicalCustomized from "./collections/LexicalCustomized";
+import LexicalDebug from "./collections/LexicalDebug";
 import Products from "./collections/Products";
 
 export default buildConfig({
-  serverURL: 'http://localhost:3001',
+  serverURL: "http://localhost:3001",
   admin: {
     user: Users.slug,
     webpack: (config) => {
@@ -23,8 +23,15 @@ export default buildConfig({
             ...config.resolve.alias,
             react: path.join(__dirname, "../node_modules/react"),
             "react-dom": path.join(__dirname, "../node_modules/react-dom"),
-            "payload": path.join(__dirname, "../node_modules/payload"),
-            '@faceless-ui/modal': path.join(__dirname, '../node_modules/@faceless-ui/modal')
+            "react-i18next": path.join(
+              __dirname,
+              "../node_modules/react-i18next"
+            ),
+            payload: path.join(__dirname, "../node_modules/payload"),
+            "@faceless-ui/modal": path.join(
+              __dirname,
+              "../node_modules/@faceless-ui/modal"
+            ),
           },
         },
       };
@@ -40,18 +47,14 @@ export default buildConfig({
     Products,
   ],
   localization: {
-    locales: [
-      'en',
-      'es',
-      'de',
-    ],
-    defaultLocale: 'en',
+    locales: ["en", "es", "de"],
+    defaultLocale: "en",
     fallback: true,
   },
   plugins: [
     //LexicalPlugin(),
   ],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts')
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
 });
