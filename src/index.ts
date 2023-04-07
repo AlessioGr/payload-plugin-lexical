2b3d7163181c62752f2ffb100d2b6c8460774dfc
Alessio Gravili <alessio@gravili.de>
2023-04-07 16:48:14 +0200

    export more stuff

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,5 @@ export * from "./types";
 export { OPEN_MODAL_COMMAND } from "./fields/LexicalRichText/plugins/ModalPlugin/index";
 export * from "./fields/LexicalRichText/ui/DropDown";
 export * from "./fields/LexicalRichText/LexicalEditorComponent";
+export * from "./fields/LexicalRichText/nodes/PlaygroundNodes";
+export * from "./fields/LexicalRichText/themes/PlaygroundEditorTheme";
