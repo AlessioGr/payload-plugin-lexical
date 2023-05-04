1acbb9a6d5867e604f144691919e6d59c8ecb110
Anthony Bouch <anthony@infonomic.io>
2023-05-05 03:13:12 +0700

    fix: fixed ModalPlugin for tables

--- src/fields/LexicalRichText/plugins/ModalPlugin/index.tsx
+++ src/fields/LexicalRichText/plugins/ModalPlugin/index.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import { useCallback, useState, ReactNode, useMemo, createContext, useContext } from 'react';
+import {
+  useCallback,
+  useState,
+  ReactNode,
+  useMemo,
+  createContext,
+  useContext,
+} from 'react';
 
 import { useConfig } from 'payload/components/utilities';
 import { formatDrawerSlug } from 'payload/dist/admin/components/elements/Drawer';
@@ -9,19 +16,25 @@ import { type SanitizedCollectionConfig } from 'payload/types';
 
 import { useModal } from '@faceless-ui/modal';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { COMMAND_PRIORITY_NORMAL, createCommand, type LexicalCommand } from 'lexical';
+import {
+  COMMAND_PRIORITY_NORMAL,
+  createCommand,
+  type LexicalCommand,
+} from 'lexical';
 
 import { type EditorConfig } from '../../../../types';
 import { useEditorConfigContext } from '../../LexicalEditorComponent';
 import { type ImagePayload } from '../../nodes/ImageNode';
-import { InsertTableDialog, InsertNewTableDialog } from '../../plugins/TablePlugin';
-import { INSERT_IMAGE_COMMAND } from '../../plugins/UploadPlugin';
-import { ListDrawer } from '../../plugins/UploadPlugin/modal';
+import { InsertTableDialog, InsertNewTableDialog } from '../TablePlugin';
+import { INSERT_IMAGE_COMMAND } from '../UploadPlugin';
+import { ListDrawer } from '../UploadPlugin/modal';
 
 export const OPEN_MODAL_COMMAND: LexicalCommand<'upload' | 'table' | string> =
   createCommand('OPEN_MODAL_COMMAND');
 
-export default function ModalPlugin(props: { editorConfig: EditorConfig }): JSX.Element {
+export default function ModalPlugin(props: {
+  editorConfig: EditorConfig;
+}): JSX.Element {
   const editorConfig = props.editorConfig;
   const [editor] = useLexicalComposerContext();
   const [activeEditor, setActiveEditor] = useState(editor);
@@ -51,22 +64,26 @@ export default function ModalPlugin(props: { editorConfig: EditorConfig }): JSX.
         toggleModal(addUploadDrawerSlug);
       } else if (toOpen === 'table') {
         const addTableDrawerSlug = formatDrawerSlug({
-          slug: `lexicalRichText-add-table-${uuid}`,
+          slug: `lexicalRichText-add-table` + uuid,
           depth: editDepth,
         });
         toggleModal(addTableDrawerSlug);
       } else if (toOpen === 'newtable') {
         const addNewTableDrawerSlug = formatDrawerSlug({
-          slug: `lexicalRichText-add-newtable-${uuid}`,
+          slug: `lexicalRichText-add-newtable` + uuid,
           depth: editDepth,
         });
         toggleModal(addNewTableDrawerSlug);
       } else {
         for (const feature of editorConfig.features) {
-          if (feature.modals != null && feature.modals.length > 0) {
+          if ((feature.modals != null) && feature.modals.length > 0) {
             for (const featureModal of feature.modals) {
               if (toOpen === featureModal.openModalCommand.type) {
-                featureModal.openModalCommand.command(toggleModal, editDepth, uuid);
+                featureModal.openModalCommand.command(
+                  toggleModal,
+                  editDepth,
+                  uuid,
+                );
                 return true;
               }
             }
@@ -76,32 +93,35 @@ export default function ModalPlugin(props: { editorConfig: EditorConfig }): JSX.
 
       return true;
     },
-    COMMAND_PRIORITY_NORMAL
+    COMMAND_PRIORITY_NORMAL,
   );
 
   // For upload
-  interface options {
-    uploads: boolean;
-  }
+  interface options { uploads: boolean }
 
   type FilteredCollectionsT = (
     collections: SanitizedCollectionConfig[],
-    options?: options
+    options?: options,
   ) => SanitizedCollectionConfig[];
-  const filterRichTextCollections: FilteredCollectionsT = (collections, options) => {
-    return collections.filter(({ admin: { enableRichTextRelationship }, upload }) => {
-      if (options?.uploads != null) {
-        return enableRichTextRelationship && Boolean(upload);
-      }
+  const filterRichTextCollections: FilteredCollectionsT = (
+    collections,
+    options,
+  ) => {
+    return collections.filter(
+      ({ admin: { enableRichTextRelationship }, upload }) => {
+        if (options?.uploads != null) {
+          return enableRichTextRelationship && Boolean(upload);
+        }
 
-      return upload != null ? false : enableRichTextRelationship;
-    });
+        return upload != null ? false : enableRichTextRelationship;
+      },
+    );
   };
 
   const uploads = true; // TODO: what does it do?
   const { collections } = useConfig();
   const [enabledCollectionSlugs] = React.useState(() =>
-    filterRichTextCollections(collections, { uploads }).map(({ slug }) => slug)
+    filterRichTextCollections(collections, { uploads }).map(({ slug }) => slug),
   );
 
   /* const [
@@ -124,7 +144,7 @@ export default function ModalPlugin(props: { editorConfig: EditorConfig }): JSX.
       });
       closeModal(addUploadDrawerSlug);
     },
-    [editor, closeModal]
+    [editor, closeModal],
   );
   const insertUpload = ({ value, relationTo, activeEditor }): void => {
     console.log('insertUpload value:', value, 'relationTo:', relationTo);
@@ -154,12 +174,13 @@ export default function ModalPlugin(props: { editorConfig: EditorConfig }): JSX.
       <InsertNewTableDialog />
 
       {editorConfig.features.map((feature) => {
-        if (feature.modals != null && feature.modals.length > 0) {
+        if ((feature.modals != null) && feature.modals.length > 0) {
           return feature.modals.map((customModal) => {
-            return customModal?.modal != null ? customModal.modal({ editorConfig }) : null;
+            return customModal?.modal != null
+              ? customModal.modal({ editorConfig })
+              : null;
           });
-        }
-        return null;
+        } return null
       })}
     </>
   );
