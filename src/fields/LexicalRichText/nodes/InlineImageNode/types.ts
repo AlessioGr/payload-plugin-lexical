a8159ee09f8de21d0f74fa2a2d169e9b6594eb07
Anthony Bouch <anthony@infonomic.io>
2023-05-12 04:33:00 +0700

    docs: updated copyright info in InlineImageNodeComponent.css

--- src/fields/LexicalRichText/nodes/InlineImageNode/types.ts
+++ src/fields/LexicalRichText/nodes/InlineImageNode/types.ts
@@ -1,16 +1,16 @@
-import type { LexicalEditor, NodeKey } from 'lexical'
+import type { LexicalEditor, NodeKey } from 'lexical';
 
-export type Position = 'left' | 'right' | 'full' | undefined
+export type Position = 'left' | 'right' | 'full' | undefined;
 
 export interface InlineImageNodePayload {
-  id: string
-  collection: string
-  src: string
-  altText: string
-  position?: Position
-  height?: number | string
-  width?: number | string
-  key?: NodeKey
-  showCaption?: boolean
-  caption?: LexicalEditor
+  id: string;
+  collection: string;
+  src: string;
+  altText: string;
+  position?: Position;
+  height?: number | string;
+  width?: number | string;
+  key?: NodeKey;
+  showCaption?: boolean;
+  caption?: LexicalEditor;
 }
